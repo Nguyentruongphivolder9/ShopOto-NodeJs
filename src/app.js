a3be2fea6e9c9c88e82e0906140b784d1d5f150e
duyhienn <duyhienn175@gmail.com>
2023-11-05 23:26:13 +0700

    test

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const path = require("path");
+const path = require('path');
 const app = express();
 const PORT = 3005;
 
-const routers = require('./routers')
+const routers = require('./routers');
 const db = require('./config/db');
 db.connect();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(expressLayouts)
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "resources", "views"));
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 routers(app);
 
-app.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`);
-})
+// app.listen(PORT, () => {
+//     console.log(`Listening on http://localhost:${PORT}`);
+// })
--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,15 +1,37 @@
 const mongoose = require('mongoose');
+const User = require('../../app/models/User');
+
+const userName = 'admingroup2';
+const password = 'dh175';
+const databaseName = 'carDb';
+const user1 = {
+    username: 'john_doe',
+    password: 'password123',
+    gender: 'male',
+    email: 'john_doe@example.com',
+    phone: '1234567890',
+    addrerss: '123 Main Street',
+    role: 'user',
+    avatar: 'john_doe.jpg',
+};
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/web_shop', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose
+            .connect(
+                `mongodb+srv://${userName}:${password}@cluster0.vjzkd85.mongodb.net/?retryWrites=true&w=majority`,
+                {
+                    dbName: databaseName,
+                },
+            )
+            .then(async () => {
+                const result = await User.find({});
+                console.log(result);
+            });
         console.log('Connect successfully!!');
     } catch (error) {
-        console.log("Connect failure!!!");
+        console.log('Connect failure!!!');
     }
 }
 
-module.exports = { connect }
+module.exports = { connect };
